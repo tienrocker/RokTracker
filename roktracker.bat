7dfb8502c5eedbeba7e15138f77634db6a789353
James Tran <tienrocker-home@gmail.com>
2022-08-28 10:04:19 +0700

    update dynamic port with scanner tag

--- roktracker.bat
+++ roktracker.bat
@@ -1,6 +1,17 @@
-".\platform-tools\adb.exe" kill-server
-".\platform-tools\adb.exe" connect localhost:53256
-cd .\
-pip install -r requirements.txt
-python roktracker.py
+@echo off
+set port=%1
+IF "%port%" == "" ( set port=55555 )
+
+netstat -o -n -a | findstr %port%
+
+if %ERRORLEVEL% equ 0 (
+    ".\platform-tools\adb.exe" kill-server
+    ".\platform-tools\adb.exe" connect localhost:%port%
+    cd .\
+    pip install -r requirements.txt
+    python roktracker.py
+) else (
+    echo "PORT %port% not open"
+)
+
 PAUSE
