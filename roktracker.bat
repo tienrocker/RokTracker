0f10f73ce1779cc2abcef4679c27b0f99c570272
James Tran <tienrocker-home@gmail.com>
2022-08-30 08:18:01 +0700

    .

--- roktracker.bat
+++ roktracker.bat
@@ -2,16 +2,10 @@
 set port=%1
 IF "%port%" == "" ( set port=55555 )
 
-netstat -o -n -a | findstr %port%
-
-if %ERRORLEVEL% equ 0 (
-    ".\platform-tools\adb.exe" kill-server
-    ".\platform-tools\adb.exe" connect localhost:%port%
-    cd .\
-    pip install -r requirements.txt
-    python roktracker.py
-) else (
-    echo "PORT %port% not open"
-)
+".\platform-tools\adb.exe" kill-server
+".\platform-tools\adb.exe" connect localhost:%port%
+cd .\
+pip install -r requirements.txt
+python roktracker.py
 
 PAUSE
